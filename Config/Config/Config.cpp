53376f8cf5e78bdc006d3f6f3dd18b7bd9bd7263
nagamie koki <nagamine.k.aa@m.titech.ac.jp>
2016-05-21 11:13:45 +0900

    ダイアログベースに変更

--- Config/Config/Config.cpp
+++ Config/Config/Config.cpp
@@ -3,27 +3,18 @@
 //
 
 #include "stdafx.h"
-#include "afxwinappex.h"
-#include "afxdialogex.h"
 #include "Config.h"
-#include "MainFrm.h"
-
-#include "ConfigDoc.h"
-#include "ConfigView.h"
+#include "ConfigDlg.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
 
-
 // CConfigApp
 
-BEGIN_MESSAGE_MAP(CConfigApp, CWinAppEx)
-	ON_COMMAND(ID_APP_ABOUT, &CConfigApp::OnAppAbout)
-	// 標準のファイル基本ドキュメント コマンド
-	ON_COMMAND(ID_FILE_NEW, &CWinAppEx::OnFileNew)
-	ON_COMMAND(ID_FILE_OPEN, &CWinAppEx::OnFileOpen)
+BEGIN_MESSAGE_MAP(CConfigApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
 END_MESSAGE_MAP()
 
 
@@ -31,15 +22,10 @@ END_MESSAGE_MAP()
 
 CConfigApp::CConfigApp()
 {
-	m_bHiColorIcons = TRUE;
-
-	// TODO: 下のアプリケーション ID 文字列を一意の ID 文字列で置換します。推奨される
-	// 文字列の形式は CompanyName.ProductName.SubProduct.VersionInformation です
-	SetAppID(_T("Config.AppID.NoVersion"));
-
 	// TODO: この位置に構築用コードを追加してください。
 	// ここに InitInstance 中の重要な初期化処理をすべて記述してください。
 }
+
 
 // 唯一の CConfigApp オブジェクトです。
 
@@ -50,13 +36,15 @@ CConfigApp theApp;
 
 BOOL CConfigApp::InitInstance()
 {
-	CWinAppEx::InitInstance();
+	CWinApp::InitInstance();
 
 
-	EnableTaskbarInteraction(FALSE);
+	// ダイアログにシェル ツリー ビューまたはシェル リスト ビュー コントロールが
+	// 含まれている場合にシェル マネージャーを作成します。
+	CShellManager *pShellManager = new CShellManager;
 
-	// RichEdit コントロールを使用するには AfxInitRichEdit2() が必要です	
-	// AfxInitRichEdit2();
+	// MFC コントロールでテーマを有効にするために、"Windows ネイティブ" のビジュアル マネージャーをアクティブ化
+	CMFCVisualManager::SetDefaultManager(RUNTIME_CLASS(CMFCVisualManagerWindows));
 
 	// 標準初期化
 	// これらの機能を使わずに最終的な実行可能ファイルの
@@ -66,111 +54,34 @@ BOOL CConfigApp::InitInstance()
 	// TODO: 会社名または組織名などの適切な文字列に
 	// この文字列を変更してください。
 	SetRegistryKey(_T("アプリケーション ウィザードで生成されたローカル アプリケーション"));
-	LoadStdProfileSettings(4);  // 標準の INI ファイルのオプションをロードします (MRU を含む)
 
+	CConfigDlg dlg;
+	m_pMainWnd = &dlg;
+	INT_PTR nResponse = dlg.DoModal();
+	if (nResponse == IDOK)
+	{
+		// TODO: ダイアログが <OK> で消された時のコードを
+		//  記述してください。
+	}
+	else if (nResponse == IDCANCEL)
+	{
+		// TODO: ダイアログが <キャンセル> で消された時のコードを
+		//  記述してください。
+	}
+	else if (nResponse == -1)
+	{
+		TRACE(traceAppMsg, 0, "警告: ダイアログの作成に失敗しました。アプリケーションは予期せずに終了します。\n");
+		TRACE(traceAppMsg, 0, "警告: ダイアログで MFC コントロールを使用している場合、#define _AFX_NO_MFC_CONTROLS_IN_DIALOGS を指定できません。\n");
+	}
 
-	InitContextMenuManager();
+	// 上で作成されたシェル マネージャーを削除します。
+	if (pShellManager != NULL)
+	{
+		delete pShellManager;
+	}
 
-	InitKeyboardManager();
-
-	InitTooltipManager();
-	CMFCToolTipInfo ttParams;
-	ttParams.m_bVislManagerTheme = TRUE;
-	theApp.GetTooltipManager()->SetTooltipParams(AFX_TOOLTIP_TYPE_ALL,
-		RUNTIME_CLASS(CMFCToolTipCtrl), &ttParams);
-
-	// アプリケーション用のドキュメント テンプレートを登録します。ドキュメント テンプレート
-	//  はドキュメント、フレーム ウィンドウとビューを結合するために機能します。
-	CSingleDocTemplate* pDocTemplate;
-	pDocTemplate = new CSingleDocTemplate(
-		IDR_MAINFRAME,
-		RUNTIME_CLASS(CConfigDoc),
-		RUNTIME_CLASS(CMainFrame),       // メイン SDI フレーム ウィンドウ
-		RUNTIME_CLASS(CConfigView));
-	if (!pDocTemplate)
-		return FALSE;
-	AddDocTemplate(pDocTemplate);
-
-
-	// DDE、file open など標準のシェル コマンドのコマンド ラインを解析します。
-	CCommandLineInfo cmdInfo;
-	ParseCommandLine(cmdInfo);
-
-
-
-	// コマンド ラインで指定されたディスパッチ コマンドです。アプリケーションが
-	// /RegServer、/Register、/Unregserver または /Unregister で起動された場合、False を返します。
-	if (!ProcessShellCommand(cmdInfo))
-		return FALSE;
-
-	// メイン ウィンドウが初期化されたので、表示と更新を行います。
-	m_pMainWnd->ShowWindow(SW_SHOW);
-	m_pMainWnd->UpdateWindow();
-	return TRUE;
+	// ダイアログは閉じられました。アプリケーションのメッセージ ポンプを開始しないで
+	//  アプリケーションを終了するために FALSE を返してください。
+	return FALSE;
 }
-
-// CConfigApp メッセージ ハンドラー
-
-
-// アプリケーションのバージョン情報に使われる CAboutDlg ダイアログ
-
-class CAboutDlg : public CDialogEx
-{
-public:
-	CAboutDlg();
-
-// ダイアログ データ
-#ifdef AFX_DESIGN_TIME
-	enum { IDD = IDD_ABOUTBOX };
-#endif
-
-protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV サポート
-
-// 実装
-protected:
-	DECLARE_MESSAGE_MAP()
-};
-
-CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
-{
-}
-
-void CAboutDlg::DoDataExchange(CDataExchange* pDX)
-{
-	CDialogEx::DoDataExchange(pDX);
-}
-
-BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
-END_MESSAGE_MAP()
-
-// ダイアログを実行するためのアプリケーション コマンド
-void CConfigApp::OnAppAbout()
-{
-	CAboutDlg aboutDlg;
-	aboutDlg.DoModal();
-}
-
-// CConfigApp のカスタマイズされた読み込みメソッドと保存メソッド
-
-void CConfigApp::PreLoadState()
-{
-	BOOL bNameValid;
-	CString strName;
-	bNameValid = strName.LoadString(IDS_EDIT_MENU);
-	ASSERT(bNameValid);
-	GetContextMenuManager()->AddMenu(strName, IDR_POPUP_EDIT);
-}
-
-void CConfigApp::LoadCustomState()
-{
-}
-
-void CConfigApp::SaveCustomState()
-{
-}
-
-// CConfigApp メッセージ ハンドラー
-
-
 
